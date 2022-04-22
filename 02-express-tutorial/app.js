07f26d4d510fc4fdf7948dc8c1643be56d3c4b37
Stefan Meza <stefanmeza93@gmail.com>
2022-04-22 18:25:48 +0200

    Add first json response

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,21 +1,11 @@
 const express = require("express");
-const path = require("path");
-
 const app = express();
+const { products } = require("./data");
 
-// setup static and middleware
-app.use(express.static("./public")); // u public folder sve sto budemo stavili toce server da ispuruci na localhost:5000, naravno mi ovaj folder ne moramo da nazovemo public to je samo neka konvencija.
-
-// app.get("/", (req, res) => {
-//   res.sendFile(path.resolve(__dirname, "./navbar-app/index.html"));
-//   adding to static assets
-//   SSR
-// });
-
-app.all("*", (req, res) => {
-  res.status(404).send("resource not found");
+app.get("/", (req, res) => {
+  res.json(products);
 });
 
 app.listen(5000, () => {
-  console.log("server is listening on port 5000");
+  console.log("Server is listening on port 5000");
 });
