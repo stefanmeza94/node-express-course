03df6e15d7a5a5cbdc6b7e36aa21861ecd1e05f4
Stefan Meza <stefanmeza93@gmail.com>
2022-04-27 15:06:25 +0200

    Add info about middleware

--- 02-express-tutorial/final/10-middleware-options.js
+++ 02-express-tutorial/final/10-middleware-options.js
@@ -1,30 +1,30 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const morgan = require("morgan");
-const logger = require("./logger");
-const authorize = require("./authorize");
+const morgan = require('morgan');
+const logger = require('./logger');
+const authorize = require('./authorize');
 //  req => middleware => res
 
 // ako zelimo da izvrsimo vise middleware funkcija za rute mozemo da prosledimo use() metodi array u koji ce da ubacimo sve middleware funkcije koje zelimo da se izvrse, naravno posle toga ce use metoda da referencira te middelware svim rutama!
-// jako je bitno da razumemo da ce biti izvrsene u ovom redosledu koji smo stavili u array.
+// jako je bitno da razumemo da ce middleware funkcije biti izvrsene u ovom redosledu koji smo stavili u array.
 app.use([logger, authorize]);
 // app.use(express.static('./public'))
 // app.use(morgan("tiny"));
 
-app.get("/", (req, res) => {
-  res.send("Home");
+app.get('/', (req, res) => {
+  res.send('Home');
 });
-app.get("/about", (req, res) => {
-  res.send("About");
+app.get('/about', (req, res) => {
+  res.send('About');
 });
-app.get("/api/products", (req, res) => {
-  res.send("Products");
+app.get('/api/products', (req, res) => {
+  res.send('Products');
 });
-app.get("/api/items", (req, res) => {
+app.get('/api/items', (req, res) => {
   console.log(req.user);
-  res.send("Items");
+  res.send('Items');
 });
 
 app.listen(5000, () => {
-  console.log("Server is listening on port 5000....");
+  console.log('Server is listening on port 5000....');
 });
